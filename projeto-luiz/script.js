a099648fb76e6b4e43071f91cf26ed4f361ace23
ItsHenrickz <canalluiz2029@gmail.com>
2025-05-25 12:04:36 -0300

    parte de Luiz Henrique

---
+++ projeto-luiz/script.js
@@ -0,0 +1,131 @@
+function abrirModal1(){
+    const modal = document.getElementById('janela-modal1')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal1'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal2(){
+    const modal = document.getElementById('janela-modal2')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal2'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal3(){
+    const modal = document.getElementById('janela-modal3')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal3'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal4(){
+    const modal = document.getElementById('janela-modal4')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal4'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal5(){
+    const modal = document.getElementById('janela-modal5')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal5'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal6(){
+    const modal = document.getElementById('janela-modal6')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal6'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal7(){
+    const modal = document.getElementById('janela-modal7')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal7'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal8(){
+    const modal = document.getElementById('janela-modal8')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal8'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal9(){
+    const modal = document.getElementById('janela-modal9')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal9'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal10(){
+    const modal = document.getElementById('janela-modal10')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal10'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal11(){
+    const modal = document.getElementById('janela-modal11')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal11'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal12(){
+    const modal = document.getElementById('janela-modal12')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal12'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
